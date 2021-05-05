1c2fe6f540f6a9ca1eb4e604a419e9f9f9cfbe43
ITanmayee <67579192+ITanmayee@users.noreply.github.com>
2021-05-05 15:41:04 +0530

    How many boundaries will you hit in total?

---
+++ Lucky_Boundaries.py
@@ -0,0 +1,15 @@
+"""
+
+You are batting at the pitch and all eyes are on you, the best player in all of IPL!
+The opposing team will relentlessly bowl at you an infinite number of balls!
+
+You notice that you only ever score a boundary on the Mth ball, if N mod M≤N/2, where N is the lucky number of your T-shirt.
+Now you wanna know how many boundaries will you hit in total?
+
+Formally, you are given a positive integer N and you need to find the number of positive integers M such that N mod M≤N/2.
+
+"""
+
+for _ in range(int(input())):
+    n = int(input())
+    print(n)
